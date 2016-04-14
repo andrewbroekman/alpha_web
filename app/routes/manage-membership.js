7f43a99d7f6620d373bff9009efe588dc4d18529
Matthias <matthiasharvey@gmail.com>
2016-04-14 22:33:32 +0200

    Fetch publications

--- app/routes/manage-membership.js
+++ app/routes/manage-membership.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	
+	model() {
+        var publications = this.store.findAll('publication'); // => GET /publications
+        return publications;
+    }
 });
 $(document).ready(function() {
     $('select').material_select();
