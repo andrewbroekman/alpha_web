6fd8e8bfa359ab144e040c2a49e9118f70bc8d5a
QuintonWeenink <qweenink@gmail.com>
2016-04-14 22:46:48 +0200

    Added basic mock objects

--- app/routes/research-group.js
+++ app/routes/research-group.js
@@ -1,12 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
-        return [
-        {name: 'Computer Science'},
-        {name: 'Artificial Intelligence'},
-        {name: 'Swarm AI'},
-        {name: 'Networking'}
-        ];
+    model() {
+        var people = this.store.findAll('research-group'); // => GET /person
+        return people;
     }
 });
--- app/routes/researcher-category.js
+++ app/routes/researcher-category.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        var people = this.store.findAll('researcher-category'); // => GET /person
+        return people;
+    }
 });
