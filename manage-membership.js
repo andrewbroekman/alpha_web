2d0af7b485728f46c3d7696faac9088b744dfa9b
StephenSwanepoel <u11032091@tuks.co.za>
2016-04-15 08:59:26 +0200

    Added files via upload

--- manage-membership.js
+++ manage-membership.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	
+	model() {
+        var members = this.store.findAll('publication'); // => GET /members
+        return members;
+    }	
 });
 $(document).ready(function() {
     $('select').material_select();
